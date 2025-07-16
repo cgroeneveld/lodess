4ac39f252369ae479c3a5bdc2ae52d5f32dc9d09
Christian Groeneveld <groeneveld@student18.strw.leidenuniv.nl>
2025-07-16 14:53:56 +0200

    part 1: set up downloading and copy of calibrators

--- LoDeSS_target.py
+++ LoDeSS_target.py
@@ -42,21 +42,47 @@ def main():
         obsid, targets = line.split(maxsplit=1)
         obsid_dict[obsid] = targets.split(',')
 
-    # Find obsids for target
-    obsids = []
-    for obsid, targets in obsid_dict.items():
-        if res["target"] in targets:
-            obsids.append(obsid)
-    print(obsids)
-    sys.exit(1)
 
     # Create a target directory
     os.mkdir(res["target"])
     os.chdir(res["target"])
 
+    # Find obsids for target
+    obsids = []
+    for obsid, targets in obsid_dict.items():
+        if res["target"] in targets:
+            obsids.append(obsid)
+    print('Calibrators required: ',obsids)
+    # Test if iranet is locally available, see LoDeSS_cal.py
+    os.mkdir('calibrators')
+    os.chdir('calibrators')
+    if not os.path.exists('/iranet/groups/ulu/c.groeneveld/LoDeSS_cal/PROGRESS/TODO/'):
+        # Download via ssh
+        for obsid in obsids:
+            # tar file first
+            cmd = f'ssh c.groeneveld@gaia.ira.inaf.it tar -C /iranet/groups/ulu/c.groeneveld/LoDeSS_cal/archive/{obsid} -cf /iranet/groups/ulu/c.groeneveld/{obsid}.tar .'
+            call(cmd, res["testing"])
+            # Copy tar file
+            cmd = f'scp -r c.groeneveld@gaia.ira.inaf.it:/iranet/groups/ulu/c.groeneveld/{obsid}.tar .'
+            call(cmd, res["testing"])
+            # delete tar file on gaia
+            cmd = f'ssh c.groeneveld@gaia.ira.inaf.it rm /iranet/groups/ulu/c.groeneveld/{obsid}.tar'
+            call(cmd, res["testing"])
+            # untar file
+            os.mkdir(obsid)
+            cmd = f'tar -xf {obsid}.tar -C {obsid}'
+            call(cmd, res["testing"])
+    else:
+        # Copy from local iranet
+        for obsid in obsids:
+            cmd = f'cp -r /iranet/groups/ulu/c.groeneveld/LoDeSS_cal/archive/{obsid} .'
+            call(cmd, res["testing"])
+    os.chdir('..')
+
     # Begin with stager
-    cmd = f'LOFAR_stager.py -t {res["target"]} -n --nobug -p LT16_014'
-    call(cmd, res["testing"])
+    for obsid in obsids:
+        cmd = f'LOFAR_stager.py -t {res["target"]} -n --nobug -p LT16_014 -o {obsid}'
+        call(cmd, res["testing"])
 
 
 if __name__ == "__main__":

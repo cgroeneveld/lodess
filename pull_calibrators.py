9994c95de8e569572c055a00c73ebda01375436c
Christian Groeneveld <groeneveld@strw.leidenuniv.nl>
2025-10-17 14:26:42 +0200

    added convenience script

---
+++ pull_calibrators.py
@@ -0,0 +1,49 @@
+import sys,glob,os
+import argparse
+
+
+def download_file_from_surf(filename):
+    # Download a file from the SURFdrive archive
+    url = f'https://surfdrive.surf.nl/index.php/s/F34o4kCVMvpcb06/download?path=%2F&files={filename}'
+    os.system(f'wget -O {filename} "{url}"')
+
+def main():
+    parser = argparse.ArgumentParser(description="Pull calibrated calibrator data from archive.")
+    parser.add_argument('pointing_name', type=str, help='Name of the pointing to process. Name should be like PXXX+XX')
+    args = parser.parse_args()
+    if not os.path.exists('obsidlist.txt'):
+        download_file_from_surf('obsidlist.txt')
+
+    # Read the obsidlist.txt file
+    with open('obsidlist.txt', 'r') as f:
+        obsidlist = f.readlines()
+    obsid_dict = {}
+    for line in obsidlist:
+        line = line.strip()
+        if not line:
+            continue
+        obsid, targets = line.split(maxsplit=1)
+        obsid_dict[obsid] = targets.split(',') # Format: OBSID: target1,target2,...
+        # OBSID is observationID, calibrators are in the archive under their observationID
+        # target1,target2,... are the names of the fields
+
+    # Find the obsids that contain the specified pointing
+    obsids = []
+    for obsid, targets in obsid_dict.items():
+        if args.pointing_name in targets:
+            obsids.append(obsid)
+
+    # Download the calibrated data for each obsid
+    for obsid in obsids:
+        filename = f'{obsid}.tar.gz'
+        print(f'Downloading {filename}...')
+        download_file_from_surf(filename)
+        print(f'Extracting {filename}...')
+        os.system(f'tar -xzf {filename}')
+        os.remove(filename)
+
+
+
+
+if __name__ == "__main__":
+    main()

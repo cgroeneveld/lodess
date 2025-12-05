e16230bc28f9aace390264e5112697c74918585c
Christian Groeneveld <groeneveld@strw.leidenuniv.nl>
2025-12-05 16:43:07 +0100

    added target iterator

---
+++ iterator_target.py
@@ -0,0 +1,30 @@
+import os,sys,glob
+import argparse
+
+
+def run_single_target(target_name):
+    os.system('LoDeSSdb.py test-connection')
+    os.system(f'LoDeSSdb.py inprog {target_name}')
+    print(f'Starting field {target_name}')
+    os.system(f'LoDeSS_target.py {target_name}')
+    os.chdir('../')
+    os.system(f'LoDeSSdb.py finished {target_name}')
+    os.system(f'LoDeSSdb.py upload {target_name}')
+    os.chdir('../../')
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s','--single',action='store_true')
+    parser.add_argument('-c','--continuous',action='store_true')
+    parser.add_argument('-f','--field',type=str)
+    args = parser.parse_args()
+    if args.single:
+        run_single_target(args.field)
+    elif args.continuous:
+        field = os.popen('LoDeSSdb.py pullrandom').read()
+        print(field)
+
+if __name__ == '__main__':
+    main()
